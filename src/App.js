e3f8ac917090fd6edbacd449ec162cada6ddf7d8
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-05-15 14:22:17 -0500

    boiler plate code

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./components/Navigation";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Project from "./components/Project";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
