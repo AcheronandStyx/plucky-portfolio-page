31c5553c45194fc46e977cb2fa84d52dad2baa05
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-05-23 23:48:25 -0500

    updtes

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 function Resume() {
-  <div>
+  return (
     <div>
-      <a href="src\assets\Resume.pdf" target="_blank">
-        Resume
-      </a>
+      <div>
+          <p>Thank you for your interst.  Please download my resumer below and reach out to me through the contact form with any inquiries.</p>
+        <a class="" href="https://github.com/AcheronandStyx/plucky-portfolio-page/blob/gh-pages/src/assets/Resume.pdf" target="_blank">
+          Resume PDF
+        </a>
+      </div>
+      <h2>Skills</h2>
+      <ul>
+        <li>HTML, JavaScript, CSS</li>
+        <li>MongoDB, SQL, Sequelize ORM</li>
+        <li>Node.js, Express.js, Handlebars.js</li>
+        <li>React</li>
+      </ul>
     </div>
-    <h1></h1>
-  </div>;
+  );
 }
 
 export default Resume;
